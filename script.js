74b6b60a6b7a08c503b6f98009d22089f551801c
Sujicoding <117893004+Sujicoding@users.noreply.github.com>
2023-07-23 12:11:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
 
+
+function getFormValue(event) {
+  event.preventDefault(); // Prevent form submission from reloading the page
+
+  // Get form elements by their IDs
+  const firstNameInput = document.getElementById('firstName');
+  const lastNameInput = document.getElementById('lastName');
+
+  // Get the values of the first and last name inputs
+  const firstName = firstNameInput.value;
+  const lastName = lastNameInput.value;
+
+  // Display the values using alert()
+  alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
 }
+
+// Add a submit event listener to the form
+const userForm = document.getElementById('userForm');
+userForm.addEventListener('submit', getFormValue);
